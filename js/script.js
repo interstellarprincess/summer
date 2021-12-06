7335d849913048c75f7162d5a74ab6aa95049d6f
interstellarprincess <54517446+interstellarprincess@users.noreply.github.com>
2021-12-05 19:52:51 -0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":null},"pages":[{"id":"page-internet-page","displayName":"internet page","link":{"linkType":"LinkTypePage","href":"#!page-internet-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-hiddenmobilepage","displayName":"hiddenmobilepage","link":{"linkType":"LinkTypePage","href":"#!page-hiddenmobilepage"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":null},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mobile","displayName":"mobile","link":{"linkType":"LinkTypePage","href":"#!page-mobile"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
