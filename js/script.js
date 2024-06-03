1a33cb4ecbc1f032de07bc9f01aee7dcaece814f
interstellarprincess <54517446+interstellarprincess@users.noreply.github.com>
2024-06-03 03:56:21 -0700

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-hi-","displayName":"hi!","link":{"linkType":"LinkTypePage","href":"#!page-hi-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
