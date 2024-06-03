92bf18f108cf6df49313c5ca2bebb0ca487ca2e1
interstellarprincess <54517446+interstellarprincess@users.noreply.github.com>
2024-06-03 03:10:22 -0700

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-hi-","displayName":"hi!","link":{"linkType":"LinkTypePage","href":"#!page-hi-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
