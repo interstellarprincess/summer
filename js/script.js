5f73d5a8ada5a78322b1c4ac7b9460a75ad4454e
interstellarprincess <54517446+interstellarprincess@users.noreply.github.com>
2024-09-12 23:43:57 -0700

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-hi-","displayName":"hi!","link":{"linkType":"LinkTypePage","href":"#!page-hi-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-art","displayName":"art","link":{"linkType":"LinkTypePage","href":"#!page-art"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
