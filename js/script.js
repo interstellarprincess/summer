834fe0641be6647cee12e79bc734181e4bdc500d
interstellarprincess <54517446+interstellarprincess@users.noreply.github.com>
2025-02-07 15:26:34 -0800

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-hi-","displayName":"hi!","link":{"linkType":"LinkTypePage","href":"#!page-hi-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-art","displayName":"art","link":{"linkType":"LinkTypePage","href":"#!page-art"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-beep","displayName":"beep","link":{"linkType":"LinkTypePage","href":"#!page-beep"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
